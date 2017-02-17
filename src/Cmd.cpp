34f3435aa757e6d55caf1e0e7f8468cbf50e92c9
Kushagra Singh <ksing009@ucr.edu>
2017-02-17 01:10:19 -0800

    Fixed error in Semicolon.cpp and implemented Cmd class

--- src/Cmd.cpp
+++ src/Cmd.cpp
@@ -1,14 +1,47 @@
 #include "../header/Base.h"
 #include "../header/Cmd.h"
 
-Cmd::Cmd() {
+//Cmd::Cmd(){
+//  this->executable = "";
+//}
 
+Cmd::Cmd(const std::string& exec,const std::vector<std::string>& aList){
+  this->executable = exec;
+  for(unsigned i = 0; i < aList.size(); i++){
+    argumentList.push_back(aList.at(i));
+  }
 }
 
 Cmd::~Cmd() {
-
 }
 
 bool Cmd::execute() {
-	return true;
+  unsigned size = (argumentList.size() + 1);
+  char* args[100]; //
+  char* file = (char*)executable.c_str();
+  
+  for(unsigned i = 0; i < size; i++){
+    args[i] = (char*)(argumentList.at(i)).c_str();
+  }
+  
+  if(size == 0){
+    args[0] = NULL;
+  }else{
+    args[size] = NULL;
+  }
+  
+  pid_t pid = fork();
+  
+  if(pid == 0){	//Child node
+    if(execvp(file,args) == -1){
+      return false;
+    }
+  }
+  if(pid > 0){ //parent
+    if( waitpid(-1,0,0) == -1){
+      return false;
+    }
+  }
+  
+  return true;
 }
