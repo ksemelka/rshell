69bad77289e386e4ce716daeed29cba5750e0771
knsingh <ksing009@ucr.edu>
2017-02-18 07:39:05 +0000

    Fix single command

--- tests/single_command.sh
+++ tests/single_command.sh
@@ -3,66 +3,26 @@ cd ../bin/
 #test case 1: test simple command with no parameters
 echo "---------------------------------------------------"
 echo "---------Test case 1: NO PARAMETERS----------------"
-echo "*********Test case 1: PROGRAM Output***************"
-./rshell.exe <<'EOF'
-ls
-EOF
-kill $!
-echo "*********Test case 1: EXPECTED Output**************"
-ls
-echo "---------------------------------------------------"
+cat ../tests/single_command_test/1.dat | ./rshell.exe > ../tests/single_command_test/1o.dat
 #test case 2: test simple command with one parameters
 echo "---------------------------------------------------"
 echo "---------Test case 2: ONE PARAMETER----------------"
-echo "*********Test case 2: PROGRAM Output***************"
-./rshell.exe <<'EOF'
-ls -a
-EOF
-kill $!
-echo "*********Test case 2: EXPECTED Output**************"
-ls -a
-echo "---------------------------------------------------"
+cat ../tests/single_command_test/2.dat | ./rshell.exe > ../tests/single_command_test/2o.dat
 #test case 3: test simple command with multiple parameters
 echo "---------------------------------------------------"
 echo "---------Test case 3: >1 PARAMETER-----------------"
-echo "*********Test case 3: PROGRAM Output***************"
-./rshell.exe <<'EOF'
-git config user.name
-EOF
-kill $!
-echo "*********Test case 3: EXPECTED Output**************"
-git config user.name
-echo "---------------------------------------------------"
+cat ../tests/single_command_test/3.dat | ./rshell.exe > ../tests/single_command_test/3o.dat
 #test case 4: test simple command with invalid parameters
 echo "---------------------------------------------------"
 echo "---------Test case 4: INVALID PARAMETER------------"
-echo "*********Test case 4: PROGRAM Output***************"
-./rshell.exe <<'EOF'
-git config user.name
-EOF
-kill $!
-echo "*********Test case 4: EXPECTED Output**************"
-ls
+cat ../tests/single_command_test/4.dat | ./rshell.exe > ../tests/single_command_test/4o.dat
 echo "---------------------------------------------------"
 #test case 5: test simple command with invalid and validparameters
 echo "---------------------------------------------------"
 echo "------Test case 5: INVALID & VALID PARAMETER-------"
-echo "*********Test case 5: PROGRAM Output***************"
-./rshell.exe <<'EOF'
-git config user.nmae
-EOF
-kill $!
-echo "*********Test case 5: EXPECTED Output**************"
-git config user.nmae
+cat ../tests/single_command_test/5.dat | ./rshell.exe > ../tests/single_command_test/5o.dat
 echo "---------------------------------------------------"
-#test case 5: test simple command with invalid command
+#test case 6: test simple command with invalid command
 echo "---------------------------------------------------"
 echo "---------Test case 6: INVALID COMMAND--------------"
-echo "*********Test case 6: PROGRAM Output***************"
-./rshell.exe <<'EOF'
-lr
-EOF
-kill $!
-echo "*********Test case 6: EXPECTED Output**************"
-lr
-echo "---------------------------------------------------"
+cat ../tests/single_command_test/6.dat | ./rshell.exe > ../tests/single_command_test/6o.dat
