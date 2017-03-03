8c843c886c617d2e52a8cf9a9ee6a007c97509dd
ksemelka <kylesemelka@gmail.com>
2017-03-03 07:54:03 +0000

    cleaned up code and added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>	//std::cout, std::endl, std::cin, std::getline
-#include <cstring>	//std::strtok, std::strstr
+#include <cstring>
 #include <vector>		//std::vector
 #include <list>
 #include <algorithm>	//std::find_first_not_of, std::find_first_of, std::find_last_not_of
@@ -22,19 +22,17 @@ int main() {
 		std::string input;
 
 		char host[100];
-     char login[100];
-     gethostname(host, 100);
-     getlogin_r(login, 100);
+      char login[100];
+      gethostname(host, 100);
+      getlogin_r(login, 100);
      
-     std::cout << login << "@" << host<< "$ ";
-		std::getline(std::cin, input);
-		input = clearCommentedCode(input);
-		input = trim(input);
-		if (input.find_first_not_of(" &|") == std::string::npos) {  // check if empty of valid characters
+      std::cout << login << "@" << host<< "$ ";
+     
+	   std::getline(std::cin, input);
+		if (input.find_first_not_of(" ") == std::string::npos) {  // check if empty of valid characters
 		   continue;
 		}
-
-		try{
+   	try {
 			root = parse(input);
 			root->execute();
 		} catch (std::exception& e) {
@@ -43,23 +41,24 @@ int main() {
 	}
 }
 
-Base* parse(const std::string& str) {
+Base* parse(const std::string& str) { 
 	std::string strcp = str;
 	Base* root;
 	std::vector<std::string> tokens;
-	if (str.find('#')) {
-	   strcp = str.substr(0, str.find('#') - 1);
-	}
 
-	Tokenize(strcp, tokens);
-	root = createTree(tokens);
+   strcp = clearCommentedCode(strcp);
+   strcp = trim(strcp);
+	tokens = Tokenize(strcp);  // Separate string into 'lines' of commands 
+	                           // lines are separated by ;
+	                           
+	root = createTree(tokens); // Create parsing tree
 
 	return root;
 }
 
 const std::string trim(const std::string& str) {
 	std::string strcp = str;
-
+	
    size_t found = strcp.find("  ");
    while (found != std::string::npos) { // Remove multiple whitespaces
 		strcp.replace(found, 2, " ");
@@ -68,16 +67,17 @@ const std::string trim(const std::string& str) {
     return strcp;
 }
 
-void Tokenize(const std::string& str,
-              std::vector<std::string>& tokens)
+std::vector<std::string>& Tokenize(const std::string& str)
 {
+	std::vector<std::string> tokens;
 	size_t startPos = 0;
-	std::string validCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~-`\'\"!#()";
+	std::string validCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~-`\'\"!#()[]_";
+	
 	for (size_t i = 0; i < str.size(); ++i) {
 		if (str.at(i) == ';') {
 			if (i == 0 || 
 				 str.find_first_of(";|&", startPos) <= str.find_first_of(validCharacters, startPos)) {
-				throw std::runtime_error("Tokenize2: Error, invalid syntax1");
+				throw std::runtime_error("from Tokenize: Error, invalid syntax1");
 			}
 			tokens.push_back(str.substr(startPos, i - startPos));
 			startPos = i + 1;
@@ -86,64 +86,71 @@ void Tokenize(const std::string& str,
 			tokens.push_back(str.substr(startPos, i - startPos + 1));
 		}
 	}
-	for (auto& a : tokens) {
-		a = trim(a);
+	
+	for (auto& a : tokens) {   // Validate syntax
 		for (size_t i = 0; i < a.size(); ++i) {
-			if (a.at(i) == '&') {
+		   
+			if (a.at(i) == '&') {   // Connectors should always be in pairs
 				if (i + 1 < a.size() && a.at(i + 1) != '&') {
-					throw std::runtime_error("Tokenize2: Error, invalid syntax2");
+					throw std::runtime_error("from Tokenize: Error, invalid syntax2");
 				}
 				++i;
 			}
 			if (a.at(i) == '|') {
 				if (i + 1 < a.size() && a.at(i + 1) != '|') {
-					throw std::runtime_error("Tokenize2: Error, invalid syntax3");
+					throw std::runtime_error("from Tokenize: Error, invalid syntax3");
 				}
 				++i;
 			}
 		}
-		if (a.find('&') == 0 || a.find('|') == 0) {
-			throw std::runtime_error("Tokenize2: Error, invalid syntax4");
+		if (a.find('&') == 0 || a.find('|') == 0) {  // Connectors should never be the first character
+			throw std::runtime_error("from Tokenize: Error, invalid syntax4");
 		}
 	}
+	//TODO: Validate brackets and parentheses
+	return tokens;
 }
 
 Base* createTree(const std::vector<std::string>& tokens) {
 	std::list<std::string> tokensList(tokens.begin(), tokens.end());
 	
 	if (tokens.size() == 0) {
-		throw std::logic_error("createTree: Error, createTree must be passed at least one token.");
+		throw std::logic_error("from createTree: Error, createTree must be passed at least one token.");
 	}
 	if (tokens.size() == 1) {
 		return _createTree(tokensList.front());
 	}
-	else {
-		Base* tempNode = NULL;
-		
-		while (!tokensList.empty()) {
-			if (tempNode) {	// leftNode already made from last iteration
-				Base* rightNode = _createTree(tokensList.front());
-				Base* newSemicolon = new Semicolon(tempNode, rightNode);
-				tempNode = newSemicolon;
-				tokensList.pop_front();
-			}
-			else {
-				Base* leftNode = _createTree(tokensList.front());
-				tokensList.pop_front();	// Get rid of tokens that we already processed
-				Base* rightNode = _createTree(tokensList.front());
-				tokensList.pop_front();
-				Base* newSemicolon = new Semicolon(leftNode, rightNode);
-				tempNode = newSemicolon;
-			}
+	
+	Base* tempNode = NULL;
+	
+	while (!tokensList.empty()) {
+		if (tempNode) {	// leftNode already made from last iteration
+			Base* rightNode = _createTree(tokensList.front());
+			Base* newSemicolon = new Semicolon(tempNode, rightNode);
+			tempNode = newSemicolon;
+			tokensList.pop_front();
 		}
-		return tempNode;
+		else {
+			Base* leftNode = _createTree(tokensList.front());
+			tokensList.pop_front();	// Get rid of tokens that we already processed
+			Base* rightNode = _createTree(tokensList.front());
+			tokensList.pop_front();
+			Base* newSemicolon = new Semicolon(leftNode, rightNode);
+			tempNode = newSemicolon;
+		}
 	}
+	return tempNode;
 }
 
 Base* _createTree(std::string s) {
 	Base* newNode = NULL;
-	s = trim(s);
-	size_t found = s.find_last_of("&|");
+
+	size_t found = s.find_last_of("])");
+	if (found != std::string::npos) {	// 
+		
+	}
+	
+   found = s.find_last_of("&|");
 		
 	if (found == std::string::npos) {	// No connectors
 		newNode = new Cmd(s);
@@ -164,18 +171,14 @@ Base* _createTree(std::string s) {
 }
 
 std::string clearCommentedCode(const std::string& input) {
-   std::string input2;
+   std::string output = input;
    if (input.find('#') != std::string::npos) {  // Truncate string if comments exist
       if (input.find('#') == 0) {
-         input2 = input;
-         input2.clear();
+         output.clear();
       }
       else {
-         input2 = input.substr(0, input.find('#'));
+         output = input.substr(0, input.find('#'));
       }
    }
-   else {
-      input2 = input;
-   }
-   return input2;
+   return output;
 }
